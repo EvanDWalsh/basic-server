41abb66b0f9061c00c1a95d2b5fa9e8a4023e75d
Spencer Brown <me@spencer.sx>
2016-11-02 16:22:12 -0400

    Basic server init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Good job!');
+});
+
+app.listen(1337, () => {
+  console.log('app listening');
+});
